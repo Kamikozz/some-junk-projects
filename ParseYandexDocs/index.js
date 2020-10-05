d37f075294a743e800e87537d89956da8bc7ad25
Alexey Shevchenko <kamikozz@mail.ru>
2020-10-06 06:33:29 +0700

    refactor: change npm description & make code more readable

--- ParseYandexDocs/index.js
+++ ParseYandexDocs/index.js
@@ -1,99 +1,63 @@
 const fs = require('fs');
 const cheerio = require('cheerio');
+const transliterate = require('./transliterate');
 
-const file = fs.readFileSync('file.html');
+const UNPARSED_RAW_HTML_DOCS_FILEPATH = 'file.html';
 
-const $ = cheerio.load(file);
-const tds = $('td').filter((idx, el) => {
+const parseHtml = (fileName) => {
+  const file = fs.readFileSync(fileName);
+  return cheerio.load(file);
+};
+
+const $ = parseHtml(UNPARSED_RAW_HTML_DOCS_FILEPATH);
+
+const getTds = () => {
+  return $('td').filter((idx, el) => {
     return el.parent.parent.parent.parent.attribs['class'] !== 'doc-c doc-c-table';
-});
+  });
+};
+
+const tds = getTds();
 
 let curValue;
-let res = [];
+let result = [];
 
 tds.each((idx, el) => {
-    switch (el.attribs['headers']) {
-        case 'regions__entry__11 ':
-        case 'regions__entry__13 ':
-            if (el.children[0]) curValue = el.children[0].data;
-            break;
-        case 'regions__entry__12 ':
-        case 'regions__entry__14 ':
-            if (el.children[0]) {
-                res.push({
-                    name: el.children[0].data,
-                    value: curValue
-                });
-            }
-            break;
-    }
+  const [firstChild] = el.children;
+
+  switch (el.attribs['headers']) {
+    case 'regions__entry__11 ':
+    case 'regions__entry__13 ':
+      if (firstChild) curValue = firstChild.data;
+      break;
+    case 'regions__entry__12 ':
+    case 'regions__entry__14 ':
+      if (firstChild) {
+        result.push({
+          name: firstChild.data,
+          value: curValue
+        });
+      }
+      break;
+    default: break;
+  }
 });
 
-res.sort((a, b) => {
-    return a['value'] - b['value'];
+result.sort(({value: valueA}, {value: valueB}) => valueA - valueB);
+
+result = result.map(({ value, name }) => {
+  return {
+    name: transliterate(name),
+    nameLocale: name,
+    value
+  }
 });
 
-let alphabet = {
-    'а': 'a',
-    'б': 'b',
-    'в': 'v',
-    'г': 'g',
-    'д': 'd',
-    'е': 'e',
-    'ё': 'yo',
-    'ж': 'zh',
-    'з': 'z',
-    'и': 'i',
-    'й': 'i',
-    'к': 'k',
-    'л': 'l',
-    'м': 'm',
-    'н': 'n',
-    'о': 'o',
-    'п': 'p',
-    'р': 'r',
-    'с': 's',
-    'т': 't',
-    'у': 'u',
-    'ф': 'f',
-    'х': 'h',
-    'ц': 'ts',
-    'ч': 'ch',
-    'ш': 'sh',
-    'щ': 'sch',
-    'ъ': '',
-    'ы': 'i',
-    'ь': '',
-    'э': 'e',
-    'ю': 'yu',
-    'я': 'ya',
-    ' ': '_'
-}
-
-res = res.map((val) => {
-    return {
-        name: transliterate(val.name),
-        nameLocale: val.name,
-        value: val.value
-    }
+result.forEach(({ nameLocale, value }) => {
+  // console.log(`{ "${nameLocale}", ${value} },`); // for C/C++
+  console.log(`<option value="${value}">${nameLocale}</option>`); // for JS
 });
 
-res.forEach((val) => {
-    //console.log(`{ "${val['nameLocale']}", ${val['value']} },`); // for C/C++
-    console.log(
-        `<option value="${val['value']}">${val['nameLocale']}</option>`); // for JS
-
-    // <option value="Красноярск">Красноярск</option>
-    //     <option value="nexus 5x">Nexus 5X</option>
-    //     <option value="galaxy s7">Galaxy S7</option>
-});
-
-// res.forEach((val) => {
-//     console.log(`${val['name']} = ${val['value']},`);
+// result.forEach((item) => {
+//     console.log(`${item['name']} = ${item['value']},`);
 // });
-
-function transliterate(str) {
-    return str.toLowerCase().split('').map((char) => {
-        return alphabet[char];
-    }).join('').toUpperCase();
-}
